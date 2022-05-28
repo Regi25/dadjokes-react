15b474bd7da47cbc8b395e05dccd56fd94d064d1
Regina Iuldasheva <reginaiuldasheva@gmail.com>
2022-05-28 13:29:35 +0200

    Second task

--- src/index.jsx
+++ src/index.jsx
@@ -1,68 +1,30 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { jokes } from './jokes.js';
+import { Joke } from './Joke/joke';
 import './style.css';
 
 const App = () => {
-  const [upLikes, setUpLikes] = useState(0);
-  const [downLikes, setDownLikes] = useState(0);
+  // const [upLikes, setUpLikes] = useState(0);
+  // const [downLikes, setDownLikes] = useState(0);
 
-  const randJoke = jokes[Math.floor(Math.random() * (jokes.length - 1))];
+  // const randJoke = jokes[Math.floor(Math.random() * (jokes.length - 1))];
 
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src={randJoke.avatar} />
-            <p className="user-name">{randJoke.name}</p>
-          </div>
-
-          <p className="joke__text">{randJoke.text}</p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={() => setUpLikes(upLikes + 1)}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {upLikes}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={() => setDownLikes(downLikes + 1)}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {downLikes}
-          </span>
-        </div>
-      </div>
+             
+      {jokes.map((joke) => (
+        <Joke
+          key={joke.id}
+          userName={joke.name}
+          userAvatar={joke.avatar}
+          text={joke.text}
+          likes={joke.likes}
+          dislikes={joke.dislikes}
+        />
+      ))}
     </div>
   );
 };
 
-render(<App />, document.querySelector('#app'));
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import './style.css';
-
-// const App = () => (
-//   <div className="container">
-//     <header>
-//       <div className="logo"></div>
-//       <h1>Webová aplikace</h1>
-//     </header>
-//     <main>
-//       <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-//     </main>
-//     <footer>
-//       <p>Czechitas, Digitální akademie: Web</p>
-//     </footer>
-//   </div>
-// );
-
-// createRoot(
-//   document.querySelector('#app')
-// ).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
